f3a7876a4d0baebb42b2a844c8e377927020eb4a
limanski-valera <102548243+limanski-valera@users.noreply.github.com>
2024-09-17 11:30:10 +0300

    add captcha

--- src/js/files/script.js
+++ src/js/files/script.js
@@ -49,8 +49,99 @@ function initPlan() {
 	document.addEventListener('mousemove', mouseMoveHandler);
 }
 
+function initForms() {
+	function setMessage(form, text, isError) {
+		let elem;
+		if (form.querySelector('.message')) {
+			elem = form.querySelector('.message');
+
+			if (elem.classList.contains('error')) elem.classList.remove('error');
+		} else {
+			elem = document.createElement('div');
+			elem.classList.add('message');
+		}
+
+		isError && elem.classList.add('error');
+		elem.textContent = text;
+
+		form.append(elem);
+	}
+
+	function removeMessage(form) {
+		const message = form.querySelector('.message');
+
+		if (message) message.remove();
+	}
+
+	function resetAllCaptchas() {
+		widgetId1 && grecaptcha.reset(widgetId1);
+		widgetId2 && grecaptcha.reset(widgetId2);
+		widgetId3 && grecaptcha.reset(widgetId3);
+		widgetId4 && grecaptcha.reset(widgetId4);
+	}
+
+	function initForm(form) {
+		form.addEventListener('submit', async (e) => {
+			e.preventDefault();
+
+			const dataObj = Object.fromEntries(new FormData(form).entries());
+
+			const isFormHasCaptcha = dataObj.hasOwnProperty('g-recaptcha-response');
+
+			if (isFormHasCaptcha && !dataObj['g-recaptcha-response']) {
+				setMessage(form, 'Пройдіть капчу', true);
+				return;
+			}
+
+			let message = '';
+
+			for (const element in dataObj) {
+				if (!dataObj[element]?.length) continue;
+
+				switch (element) {
+					case 'name':
+						message += `Імʼя: ${dataObj[element]}\n`;
+						break;
+					case 'phone':
+						message += `Телефон: ${dataObj[element]}\n`;
+						break;
+					case 'email':
+						message += `Email: ${dataObj[element]}\n`;
+						break;
+					case 'message':
+						message += `Повідомлення: ${dataObj[element]}\n`;
+						break;
+					case 'g-recaptcha-response':
+						break;
+					default:
+						message += dataObj[element] + '\n';
+				}
+			}
+
+			setMessage(form, 'Незабаром ми з вами звʼяжемося!');
+
+			form.reset();
+			resetAllCaptchas();
+
+			setTimeout(() => {
+				removeMessage(form);
+			}, 2000);
+		});
+	}
+
+	const forms = document.querySelectorAll('form');
+
+	forms.forEach((form) => {
+		initForm(form);
+	});
+}
+
 document.addEventListener('DOMContentLoaded', () => {
 	if (document.querySelector('.general-plan')) {
 		initPlan();
+	}
+
+	if (document.querySelector('form')) {
+		initForms();
 	}
 });
